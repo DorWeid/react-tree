5531a8f53237df808d11c274be11f0ce86263f76
DorWeid <dorprozor@gmail.com>
2016-11-28 16:26:45 +0200

    Refactored utils using consts

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,30 @@
 export const DEFAULT_COLOR = "#61DAFB";
-export const DIRTY_COLOR = "#fb0100";
+export const DIRTY_COLOR = "#FB0100";
+
+const STARTING_HEIGHT = 100;
+const DECREASE_PER_LEVEL = 10;
+const NODE_REF_NAME = 'node';
+const X_OFFSET = 570;
+const Y_OFFSET = 20;
 
 const mapNodeToState = (node, height) => ([{
   data: node.key,
   nodeKey: node.key,
   rightSon: node.rightChild,
   leftSon: node.leftChild,
-  size: 100 - height * 10,
+  size: STARTING_HEIGHT - height * DECREASE_PER_LEVEL,
   height: height,
-  ref: `node${node.key}`,
+  ref: `${NODE_REF_NAME}${node.key}`,
   isDirty: false
 }]);
 
-export const getNodesArray = (node, height = 1) => {
+/**
+ * Return the AVL tree as an array in an in-order
+ * @param node
+ * @param height
+ * @returns {Array}
+ */
+export const getNodesArray = (node , height = 1) => {
   if(!node) return [];
 
   return getNodesArray(node.leftChild, height + 1)
@@ -20,7 +32,12 @@ export const getNodesArray = (node, height = 1) => {
     .concat(getNodesArray(node.rightChild, height + 1));
 };
 
-export const getLinesArray = (nodesArray) => {
+/**
+ * Create the edges array using vertexes(nodes) array
+ * @param nodesArray
+ * @returns {Array}
+ */
+export const getLinesArray = (nodesArray = []) => {
   const arr = [];
   let key = 1;
 
@@ -30,10 +47,10 @@ export const getLinesArray = (nodesArray) => {
         ((nodesArray[i].rightSon && nodesArray[i].rightSon.key === nodesArray[j].nodeKey) ||
         (nodesArray[i].leftSon && nodesArray[i].leftSon.key === nodesArray[j].nodeKey ))) {
         arr.push({
-          x1: nodesArray[i].x + 570,
-          x2: nodesArray[j].x + 570,
-          y1: nodesArray[i].y + 20,
-          y2: nodesArray[j].y + 20,
+          x1: nodesArray[i].x + X_OFFSET,
+          x2: nodesArray[j].x + X_OFFSET,
+          y1: nodesArray[i].y + Y_OFFSET,
+          y2: nodesArray[j].y + Y_OFFSET,
           key: key++,
         });
       }
